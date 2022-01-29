d99dca4a98337c2e7a074cdc41d412dc96593aa9
kinane00 <kinanemohammed@gmail.com>
2022-01-29 15:40:03 +0100

    CQRS Mediator Module

--- API/Startup.cs
+++ API/Startup.cs
@@ -2,6 +2,13 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.OpenApi.Models;
 using Persistence;
+using Microsoft.Extensions.DependencyInjection;
+using MediatR;
+using System.Collections.Generic;
+using Application.Activities;
+using Application;
+using AutoMapper;
+using Application.Core;
 
 namespace API
 {
@@ -34,6 +41,9 @@ namespace API
                     policy.AllowAnyMethod().AllowAnyHeader().WithOrigins("http://localhost:3000");
                 });
             });
+            
+            services.AddMediatR(typeof(List.Handler).Assembly);
+            services.AddAutoMapper(typeof(MappingProfiles).Assembly);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
